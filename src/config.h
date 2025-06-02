a6a30aafd969438c3a0263083d77e46d644ff7a0
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-06-02 06:56:52 +0200

    Started with config.h

---
+++ src/config.h
@@ -0,0 +1,89 @@
+// Libraries
+#include "types/customized.c"
+#include <stddef.h>
+
+/// # Config file for jazzy
+/// This file defines various constans for the
+/// jazzy installer such as config source and destination
+/// and more..
+///
+/// File paths are relative to $HOME and cannot start with '/'
+
+
+/// ## Config files
+/// Move everything in $HOME/Jazzian/dotfiles/config/ into $HOME/.config/
+static const char *config_src = "Jazzian/dotfiles/config/";
+static const char *config_dest = ".config/";
+
+/// ## Scripts and executables
+/// Move everything in $HOME/Jazzian/dotfiles/bin into $HOME/.local/bin/
+static const char *bin_src = "Jazzian/dotfiles/bin/";
+static const char *bin_dest = ".local/bin/";
+
+/// ## Customized files
+/// ### Predifined file contens
+#define customized_sh "killshells() {\n" \
+    "    pkill -KILL -u $USER -t tty1\n" \
+    "}\n" \
+    "export BROWSER_PREFIX=\"firefox\"\n" \
+    "[ \"$(tty)\" = \"/dev/tty1\" ] && (startx; killshells)\n"
+
+#define debmdmenu "#!/usr/bin/sh\n" \
+                  "if [ $XDG_SESSION_TYPE = \"wayland\" ]; then\n" \
+                  "    exec wofi_dmenu;\n" \
+                  "else\n" \
+                  "    exec rofi_dmenu;\n" \
+                  "fi\n"; \
+
+#define debmdrun "#!/usr/bin/sh\n" \
+                 "if [ $XDG_SESSION_TYPE = \"wayland\" ]; then\n" \
+                 "    exec wofi_app;\n" \
+                 "else\n" \
+                 "    exec rofi_app;\n" \
+                 "fi\n" \
+
+#define mdmenu_content "#!/usr/bin/sh\n" \
+                       "exec rofi_dmenu\n" \
+
+#define mdrun_content "#!/usr/bin/sh\n" \
+                      "exec rofi_app\n" \
+
+#define myterm_content "#!/bin/sh\n" \
+                       "case $XDG_SESSION_TYPE in\n" \
+                       "    \"wayland\")\n" \
+                       "        exec alacritty -o font.size=12 $@\n" \
+                       "        ;;\n" \
+                       "    *)\n" \
+                       "        exec alacritty -o font.size=12 $@\n" \
+                       "        ;;\n" \
+                       "esac\n" \
+
+#define startx_content "x11startup &\nexec i3\n"
+
+#define x11startup "#!/usr/bin/bash\n"
+
+
+#define gtk3_config "[Settings]\n" \
+                    "gtk-theme-name=Adwaita-dark\n" \
+                    "gtk-icon-theme-name=Papirus-Dark\n" \
+                    "gtk-font-name=JetBrains Mono Light 12\n" \
+                    "gtk-cursor-theme-size=0"
+
+
+
+/// These files will be created by the installer
+static const Customized custom_files[] = {
+   {.path = ".customenv", .content = "", .permission = 0644},
+   {.path = ".customized.sh", .content = customized_sh, .permission = 0644}
+};
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/types/customized.c
@@ -0,0 +1,9 @@
+// Libraries
+#include <sys/types.h>
+
+
+typedef struct {
+    const char *path;
+    const char *content;
+    const mode_t permission;
+} Customized;
