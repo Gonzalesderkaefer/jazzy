9c6d05148590ad94dcb83415ff0f021b7463c92b
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-06-05 18:59:51 +0200

    Customized: added create_customized() function

--- src/customized.c
+++ src/customized.c
@@ -3,13 +3,53 @@
 #include "headers/types/customized.h"
 
 // Libraries
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "headers/utils/fileutils.h"
+#include "headers/utils/path.h"
 
 
 void create_customized(const char *path, const char *content, const mode_t perm) {
+    // Check if path exists
+    if (!access(path, F_OK))
+        return;
+
+    puts(path);
+
     // check if path is directory
     if (path[strlen(path) - 1] == '/') {
-        mkdir_p(path, perm);
+        mkdir_p(path, perm); // create all directories
+        return;
     }
+
+    // Create new path string
+    Path *new_path = path_init(path);
+    if (!new_path) { // Error checking
+        return;
+    }
+
+    // Get parent of new_path to create the path
+    const char *new_parent = path_parent(new_path);
+
+    // Create new_path's parent dir
+    if (mkdir_p(new_parent, perm) == -1) { // Error checking
+        path_free(new_path); // We have to free this...
+        return;
+    }
+
+    // Open file
+    FILE *pathfp = fopen(path, "w");
+    if (!pathfp) { // Error checking
+        path_free(new_path); // We have to free this...
+        return;
+    }
+
+    // Write to the file 
+    fwrite(content, sizeof(char) , strlen(content), pathfp);
+
+
+    // Cleanup
+    fclose(pathfp);
+    path_free(new_path);
 }
