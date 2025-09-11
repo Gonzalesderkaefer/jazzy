d12bfad206c1668e51e513f1b327fec47a4cdd92
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-09-11 22:02:36 +0200

    skeleton for copy_dir

--- src/utils/fileutils.rs
+++ src/utils/fileutils.rs
@@ -1,5 +1,6 @@
 // Libraries
 use std::path::Path;
+use std::fs;
 
 /// Copy a directory from `from` to `to`.
 ///
@@ -12,4 +13,12 @@ pub fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) {
     // Save from and to as (refs to) paths
     let from_path = from.as_ref();
     let to_path = to.as_ref();
+
+    // Check if to exists
+    if ! to_path.exists() {
+        // Create to and recursively call function
+        fs::create_dir_all(to_path);
+        copy_dir(from_path, to_path);
+    }
+
 }
