150972515677aa40c393810b93b36fec0c9da36d
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-09-11 21:44:24 +0200

    Started with file utils

---
+++ src/utils/fileutils.rs
@@ -0,0 +1,15 @@
+// Libraries
+use std::path::Path;
+
+/// Copy a directory from `from` to `to`.
+///
+/// The following cases will be considered:  
+///  * `to` exists, then `from` will be created as `to`'s child directory
+///  * `to` does not exist, then to will become the new dir name i.e it will
+///     be created and the contents of `from` will be copied into `to`
+///  * `to` and `from` point to the same directory, then no action is taken
+pub fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) {
+    // Save from and to as (refs to) paths
+    let from_path = from.as_ref();
+    let to_path = to.as_ref();
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,3 @@
 pub mod menu;
 pub mod formatting;
+pub mod fileutils;
