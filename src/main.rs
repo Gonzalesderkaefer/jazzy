c6332f075604cd9668c04dc02ac6253e76a40ac1
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-09-12 10:50:18 +0200

    first draft of copy_dir()

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod display_server;
 mod config;
 mod utils;
 
-
+use utils::fileutils as fu;
 
 fn main() {
 }
--- src/utils/fileutils.rs
+++ src/utils/fileutils.rs
@@ -1,9 +1,6 @@
 // Libraries
 use std::path::Path;
-use std::fs;
-use std::io;
-
-/// This struct represents an Error for this module
+use std::fs; use std::io; /// This struct represents an Error for this module
 pub struct FileUtilErr {}
 
 
@@ -15,20 +12,28 @@ pub struct FileUtilErr {}
 ///  * `to` exists, then `from` will be created as `to`'s child directory
 ///  * `to` does not exist, then to will become the new dir name i.e it will
 ///     be created and the contents of `from` will be copied into `to`
-///  * `to` and `from` point to the same directory, then no action is taken
+///  * `to` does not exist, then no action is taken
 pub fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> io::Result<()> {
     // Save from and to as (refs to) paths
     let from_path = from.as_ref();
     let to_path = to.as_ref();
 
+    // Check if from exists
+    if ! from_path.exists() {
+        return Ok(());
+    }
+
     // Check if to exists
     if ! to_path.exists() {
-        // Create to and recursively call function
+        // Create to
         let create_dir_result = fs::create_dir_all(to_path);
         match (create_dir_result) {
             Ok(none) => {},
             Err(e) => { return Err(e); },
         }
+        // call the function recursively
+        let copy_result = copy_dir(from_path, to_path);
+        return copy_result;
     }
 
     // Open from_dir
@@ -51,11 +56,31 @@ pub fn copy_dir<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> io::Result<()
             Err(e) => { return Err(e); },
         };
 
+        // Get the basename of the opened dir
+        let opened_basename = opened_entry.file_name();
+
+        // Build new source directory
+        let mut source_dir_path = from_path.to_path_buf();
+        source_dir_path.push(&opened_basename);
+
+        // Build new destination directory
+        let mut dest_dir_path = to_path.to_path_buf();
+        dest_dir_path.push(&opened_basename);
+
         // check if opened_entry is a directory
         if opened_file_type.is_dir() {
-            let opened_basename = opened_entry.file_name();
-
+            // Call the function recursively
+            match copy_dir(source_dir_path.as_path(), dest_dir_path.as_path()) {
+                Ok(()) => {},
+                Err(e) => { return Err(e); },
+            }
+        } else {
+            match fs::copy(source_dir_path.as_path(), dest_dir_path.as_path()) {
+                Ok(num) => {},
+                Err(e) => { return Err(e); },
+            }
         }
+
     }
 
 
